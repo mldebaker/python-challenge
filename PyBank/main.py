5f7b1885ede4f657563e95cd4d3659dab750d22c
mldebaker <mldebaker@gmail.com>
2019-09-11 23:54:09 -0600

    Code for homework

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,23 @@
 # Homework Start Up
+# Import csv and os
+import os
+import csv
+
+# Set cvs Path
+csvpath = os.path.join('..', 'PyBank', 'main.py')
+
+# Set Variables
+total_variables = []
+net_total = []
+
+# Print Title 
+print("Financial Analysis")
+print("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ")
+
+# 
+with open(csvpath, 'r', newline='', encoding='utf8') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+# Define Variables
+    for row in csvreader:
+        total_variables.append(row[1]
